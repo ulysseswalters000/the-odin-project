9789d09c9e74499bad4b7d8723c065c0120c1fab
andrew walters <ulysseswalters000@gmail.com>
2022-03-20 00:08:06 -0400

    init math playground

---
+++ foundations/javascript/math/js/app.js
@@ -0,0 +1,12 @@
+// testing some math jawns
+const myInt = 5;
+const myFloat = 0.667;
+const myAllegedAge = 95;
+const salary = 150000;
+
+console.log(`My preferred salary is ${salary}`)
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2)
+console.log(twoDecimalPlaces)
