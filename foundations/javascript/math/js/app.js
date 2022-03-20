58b2ee443ab911e092f771aae70b7f4d61699373
andrew walters <ulysseswalters000@gmail.com>
2022-03-20 00:12:08 -0400

    explicit string to number conversion

--- foundations/javascript/math/js/app.js
+++ foundations/javascript/math/js/app.js
@@ -10,3 +10,16 @@ const lotsOfDecimal = 1.766584958675746364;
 lotsOfDecimal;
 const twoDecimalPlaces = lotsOfDecimal.toFixed(2)
 console.log(twoDecimalPlaces)
+
+// convert string repr of number to number
+let myNumber = '74';
+myNumber += 3;
+console.log(myNumber);
+console.log(typeof myNumber);
+
+// cast the original string to a number type and check it
+// the math works now as intended
+myNumber = Number('74');
+myNumber += 3;
+console.log(myNumber)
+console.log(typeof myNumber)
