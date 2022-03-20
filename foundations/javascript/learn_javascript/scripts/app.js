62d045dd8e9a99b1dc985ce60ad87ebb58bbc583
andrew walters <ulysseswalters000@gmail.com>
2022-03-19 23:59:54 -0400

    init javascript learning

---
+++ foundations/javascript/learn_javascript/scripts/app.js
@@ -0,0 +1,26 @@
+// let declarations are mutable
+let name = 'Andrew', age = 32, message = 'Hello';
+
+console.log(`${message} ${name}, your age is ${age}`);
+
+// const declarations are immutable
+const bDay = '11.14.1989';
+
+//  convention often has declarations as all uppercase with snake case
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+// tasks
+name = 'John';
+let admin = name;
+;
+alert(name);
+
+let planet = 'Earth';
+let currUser;
